bc40814ae22baaf2714c421d22c465dbfad1f4b0
jolien21 <jolien.mesure@hotmail.com>
2025-10-15 15:43:36 +0200

    juiste test

--- src/main/java/be/vdab/goededoel/GoedeDoel.java
+++ src/main/java/be/vdab/goededoel/GoedeDoel.java
@@ -4,7 +4,7 @@ import java.math.BigDecimal;
 //com
 public class GoedeDoel {
 	private final String naam;
-	private BigDecimal opbrengst;
+	private BigDecimal opbrengst =  BigDecimal.ZERO;
 
 	public GoedeDoel(String naam) {
 		this.naam = naam;
